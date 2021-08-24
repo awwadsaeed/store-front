0db54abb3a3a631c266c3dd3d7f096bf6c9c01d9
awwadsaeed <saeedawwad450@gmail.com>
2021-08-24 14:33:54 +0300

    fixes to the category

--- src/async-func/thunk.js
+++ src/async-func/thunk.js
@@ -1,4 +1,5 @@
 import { CompassCalibrationOutlined } from "@material-ui/icons";
+export const getRemoteData = () => {
     return async (dispatch) => {
         const raw = await fetch('https://api-js401.herokuapp.com/api/v1/products');
         const results = await raw.json();
