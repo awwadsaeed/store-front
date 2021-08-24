f9788f09335475ef8a7dfc44e3362ac7a029e2bd
awwadsaeed <saeedawwad450@gmail.com>
2021-08-24 14:23:57 +0300

    stuff

--- src/async-func/thunk.js
+++ src/async-func/thunk.js
@@ -1,7 +1,4 @@
 import { CompassCalibrationOutlined } from "@material-ui/icons";
-
-// Thunk will give us the ability to return a function that will have access to dispach
-export const getRemoteData = () => {
     return async (dispatch) => {
         const raw = await fetch('https://api-js401.herokuapp.com/api/v1/products');
         const results = await raw.json();
